fbba09c7b1e8c9d7c2549d0cd2d007df25fdd0ca
Ivan <ivanrakov540@gmail.com>
2025-06-11 03:30:40 +0500

    fix: cnahge sheet numbers display

--- src/apps/meetings/domain/schemas/question.py
+++ src/apps/meetings/domain/schemas/question.py
@@ -1,4 +1,5 @@
 from typing import Literal
+from pydantic import field_serializer
 
 from apps.meetings.domain.schemas.base import BaseSchema
 from apps.meetings.domain.schemas.tag import TagResponse
@@ -21,5 +22,11 @@ class QuestionResponse(BaseSchema):
     author_classification: Literal[*AuthorClassification.values, '']
     solution: Literal[*Solution.values, '']
     solution_content: str = ''
-    sheet_numbers: list[float]
+    sheet_numbers: str
     tags: list[TagResponse]
+
+    @field_serializer('sheet_numbers')
+    def convert_sheet_numbers(self, sheet_numbers: list[str]):
+        if sheet_numbers[0] == sheet_numbers[1]:
+            return sheet_numbers[0].replace('.5', ' об')
+        return ' - '.join(number.replace('.5', ' об') for number in sheet_numbers)
